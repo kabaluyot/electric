371b6a29c5400729d74f1ac6d0abaec9d1bd4636
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-01-13 07:33:52 +0800

    add type to win

--- src/background.ts
+++ src/background.ts
@@ -8,7 +8,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win:any
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true, standard: true } }])
