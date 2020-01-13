dcbaaa42392f4076d24fa9c5ad02786944ba2c0e
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-01-13 08:07:00 +0800

    add vue router support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
+Vue.config.devtools = process.env.NODE_ENV === 'development'
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
