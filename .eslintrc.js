b170f422684827f3b22a67246a8c48a13cbdc700
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-02-29 20:43:56 +0800

    use typescript-eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
     ecmaFeatures: {
       legacyDecorators: true
     }
